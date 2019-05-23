208bb05d6162bb96bf8fdf94058ef52fd3e4e19b
tjdds1109 <tjdds1109@naver.com>
2019-05-23 14:57:24 +0900

    travis.yml (ver2) +

--- src/test/java/com/yeonbot/roommate/member/MemberControllerTest.java
+++ src/test/java/com/yeonbot/roommate/member/MemberControllerTest.java
@@ -1,14 +1,25 @@
 package com.yeonbot.roommate.member;
 
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -26,23 +37,23 @@ public class MemberControllerTest {
                 .build();
     }
 
-//    @Test
-//    public void loginTest() throws Exception {
-//        AuthenticationRequest request = new AuthenticationRequest();
-//        request.setMemberId("tjddus1109");
-//        request.setPassword("1234");
-//
-//        //ObjectMapper -> JSON 위한 객체
-//        ObjectMapper om = new ObjectMapper();
-//
-//        mvc.perform(post("/member/login")
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                .content(om.writeValueAsString(request)))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andExpect(jsonPath("$.memberId", is(request.getMemberId())))
-//                .andExpect(jsonPath("$.authorities[*].roleName", hasItem("USER")))
-//        ;
-//    }
+    @Test
+    public void loginTest() throws Exception {
+        AuthenticationRequest request = new AuthenticationRequest();
+        request.setMemberId("tjddus1109");
+        request.setPassword("1234");
+
+        //ObjectMapper -> JSON 위한 객체
+        ObjectMapper om = new ObjectMapper();
+
+        mvc.perform(post("/member/login")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(om.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.memberId", is(request.getMemberId())))
+                .andExpect(jsonPath("$.authorities[*].roleName", hasItem("USER")))
+        ;
+    }
 
 }
