52db8969d52ace6c671ce489e4f1258d402f38f8
tjdds1109 <tjdds1109@naver.com>
2019-05-27 19:07:43 +0900

    add getRoomsInfo method

--- src/main/java/com/yeonbot/roommate/roomPreView/RoomController.java
+++ src/main/java/com/yeonbot/roommate/roomPreView/RoomController.java
@@ -1,16 +1,20 @@
 package com.yeonbot.roommate.roomPreView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import lombok.AllArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -23,6 +27,19 @@ public class RoomController {
     @RequestMapping("/test")
     public String hello(){
         return "Hello Wrold !";
+    }
+
+    @GetMapping("/Rooms")
+    public String getRoomsInfo(){
+        Gson gson = new Gson();
+        List<Room> rooms = roomRepository.findAll();
+        JsonArray jsonArray = new JsonArray();
+
+        rooms.forEach((item)->{
+            String str = gson.toJson(item);
+            jsonArray.add(str);
+        });
+        return jsonArray.toString();
     }
 
     @RequestMapping("/crawling")
